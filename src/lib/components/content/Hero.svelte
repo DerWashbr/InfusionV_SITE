174c11b7b981f06957dce92fa2ac8570b71c4d76
DerWashbr <57878304+DerWashbr@users.noreply.github.com>
2023-11-29 07:47:02 +0100

    chore: remove backend, added Loginlogic

---
+++ src/lib/components/content/Hero.svelte
@@ -0,0 +1,47 @@
+
+
+<section class="text-gray-400 body-font bg-background">
+  <div class="container px-5 py-24 mx-auto">
+    <div class="flex flex-wrap w-full mb-20">
+      <div class="lg:w-1/2 w-full mb-6 lg:mb-0">
+        <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">Warum InfusionV?</h1>
+        <div class="h-1 w-20 bg-royalblue rounded"></div>
+      </div>
+      <p class="lg:w-1/2 w-full leading-relaxed text-gray-400 text-opacity-90">Willkommen in einer Welt, in der deine Träume und Ambitionen keine Grenzen kennen. Unser maßgeschneiderter GTA 5 Roleplay Server öffnet die Tore zu einem unvergleichlichen Abenteuer, das dich direkt ins Herz von Los Santos entführt. Warum bei uns spielen?</p>
+    </div>
+    <div class="flex flex-wrap -m-4">
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content">
+          <h3 class="tracking-widest text-royalblue text-xs font-medium title-font">Sei wer, du sein willst!</h3>
+          <h2 class="text-lg text-white font-medium title-font mb-4">Einzigartiges RP-Erlebnis</h2>
+          <p class="leading-relaxed text-base">Erstelle deinen eigenen Charakter, forme seine Geschichte und interagiere mit einer lebendigen Gemeinschaft von Gleichgesinnten. Ob du ein gesetzestreuer Bürger, ein gewiefter Geschäftsmann oder ein eiskalter Gangster sein willst – die Entscheidung liegt bei dir.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content">
+          <h3 class="tracking-widest text-royalblue text-xs font-medium title-font">Nie hungrig!</h3>
+          <h2 class="text-lg text-white font-medium title-font mb-4">Los Santos</h2>
+          <p class="leading-relaxed text-base">Unser Server bietet eine Fülle von Aktivitäten und Berufen. Von Polizisten über Unternehmer bis hin zu Straßenkünstlern – finde deine Berufung und gestalte dein Spielerlebnis nach deinen Vorstellungen. Unser Wirtschaft orientiert sich realtitätsnah.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content">
+          <h3 class="tracking-widest text-royalblue text-xs font-medium title-font">Partys & Events?</h3>
+          <h2 class="text-lg text-white font-medium title-font mb-4">Mit der Community</h2>
+          <p class="leading-relaxed text-base">Trete einer lebhaften Gemeinschaft bei, die Events, Wettbewerbe und regelmäßige Updates organisiert. Unser Ziel ist es, deine Spielerfahrung ständig zu verbessern und spannende Möglichkeiten zu bieten, um mit anderen Spielern zu interagieren.</p>
+        </div>
+      </div>
+      <div class="xl:w-1/4 md:w-1/2 p-4">
+        <div class="bg-gray-800 bg-opacity-40 p-6 rounded-lg">
+          <img class="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content">
+          <h3 class="tracking-widest text-royalblue text-xs font-medium title-font">Das Team</h3>
+          <h2 class="text-lg text-white font-medium title-font mb-4">Und du!</h2>
+          <p class="leading-relaxed text-base">Unser Team steht bereit, um sicherzustellen, dass dein Aufenthalt reibungslos verläuft. Egal, ob du Hilfe bei technischen Problemen oder Unterstützung bei der Anpassung deiner Spielerfahrung benötigst – wir sind für dich da.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
---
+++ src/routes/rules/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import Seo from '$components/Seo.svelte';
+	import Rules from '$components/content/Rules.svelte';
+</script>
+
+<Seo />
+
+<svelte:head>
+	<title>InfusionV - More than Roleplay</title>
+	<link rel="canonical" href="https://infusionV.de" />
+</svelte:head>
+
+<div class="card shadow-lg">
+	<div class="card-body">
+		<center><div class="rules-desc">Unsere Regeln</div> </center>
+	</div>
+</div>
+<Rules />
+
+<style>
+	.rules-desc {
+			/* Coming Soon, */
+    font-family: Lucida Grande, Lucida Sans Unicode, Lucida Sans, DejaVu Sans, Verdana, 'sans-serif';
+    font-weight: 400;
+    line-height: 1.15em;
+    margin: 0 0 15px;
+    color: #4169e1;
+    font-size: 26px;
+    position: relative;
+    left: -12px;
+	}
+</style>
